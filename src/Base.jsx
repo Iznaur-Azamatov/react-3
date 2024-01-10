2e1d286a0d1e0c8c583530700336ed6e1a4be22b
Azamatov Iznaur <azamatoviznaur@gmail.com>
2024-01-10 12:10:45 +0300

    added README.md

---
+++ src/Base.jsx
@@ -0,0 +1,13 @@
+import Img from "./image";
+
+function Base() {
+    return (
+      <div>
+       <h1>Hello World!</h1>
+       <Img/>
+      </div>
+    );
+  }
+  
+  export default Base;
+  
---
+++ src/image.jsx
@@ -0,0 +1,13 @@
+import image from './img/Rectangle 12.png';
+
+
+function Img() {
+    return (
+      <div>
+       <img src={image}/>
+      </div>
+    );
+  }
+  
+  export default Img;
+  
